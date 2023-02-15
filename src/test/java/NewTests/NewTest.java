9837ed8820a2f430f749464b8970193908a8ff9d
Andrew Bartenev <abartenev@rvision.ru>
2023-02-15 22:15:19 +0300

    Add new test

---
+++ src/test/java/NewTests/NewTest.java
@@ -0,0 +1,13 @@
+package NewTests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class NewTest {
+
+    @Test
+    void newTest() {
+        open("https://github.com");
+    }
+}
